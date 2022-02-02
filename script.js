9078fe0db8bfc7ccdfb5729ef97d30879043463c
Gabriel Silva <gabriel.nascimento@koin.com.br>
2022-02-02 13:36:39 -0300

    show current video and capture an area

--- script.js
+++ script.js
@@ -1,46 +1,72 @@
-window.onload = () => {
-  function gotMedia(mediaStream) {
-    const mediaStreamTrack = mediaStream.getVideoTracks()[0];
-    console.log(mediaStream.getVideoTracks())
-    const imageCapture = new ImageCapture(mediaStreamTrack);
-    return imageCapture;
-  }
+function readBarCode(blob) {
+  if (!("BarcodeDetector" in window)) {
+    console.log("Barcode Detector is not supported by this browser.");
+  } else {
+    console.log("Barcode Detector supported!");
 
-  function readBarCode(blob) {
-    if (!("BarcodeDetector" in window)) {
-      console.log("Barcode Detector is not supported by this browser.");
-    } else {
-      console.log("Barcode Detector supported!");
+    // create new detector
+    var barcodeDetector = new BarcodeDetector({
+      formats: [
+        "code_39",
+        "codabar",
+        "ean_13",
+        "qr_code",
+        "data_matrix",
+        "aztec",
+      ],
+    });
 
-      // create new detector
-      var barcodeDetector = new BarcodeDetector({
-        formats: ["code_39", "codabar", "ean_13", "qr_code", "data_matrix", "aztec"],
-      });
+    barcodeDetector
+      .detect(blob)
+      .then((barcodes) => {
+        console.log("barcodes foundeds");
+        console.log(barcodes);
 
-      barcodeDetector
-        .detect(blob)
-        .then((barcodes) => {
-          console.log("barcodes foundeds");
-          console.log(barcodes);
+        barcodes.forEach((barcode) => {
+          console.log(barcode.rawData);
 
-          barcodes.forEach((barcode) => {
-            console.log(barcode.rawData);
-
-            document.getElementById("barcode-data").innerHTML = barcode.rawData;
-          });
-        })
-        .catch((err) => {
-          console.log(err);
+          document.getElementById("barcode-data").innerHTML = barcode.rawData;
         });
-    }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
+}
 
-  document.getElementById("capture").addEventListener("click", () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then((mediaStream) => {
-        console.log(mediaStream)
-        const imageCapture = gotMedia(mediaStream);
+function gotMedia(mediaStream) {
+  const mediaStreamTrack = mediaStream.getVideoTracks()[0];
+  console.log(mediaStream.getVideoTracks());
+  const imageCapture = new ImageCapture(mediaStreamTrack);
+  return imageCapture;
+}
+
+function captureVideo() {
+  var video = document.getElementById("video");
+  video.setAttribute("playsinline", "");
+  video.setAttribute("autoplay", "");
+  video.setAttribute("muted", "");
+  video.style.width = "200px";
+  video.style.height = "200px";
+
+  /* Setting up the constraint */
+  var constraints = {
+    audio: false,
+    video: {
+      facingMode: { exact: "environment" },
+      // facingMode: "user",
+    },
+  };
+
+  /* Stream it to video element */
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function success(stream) {
+      console.log(stream);
+      video.srcObject = stream;
+
+      document.getElementById("capture").addEventListener("click", () => {
+        const imageCapture = gotMedia(stream);
         const image = document.getElementById("imageEl");
 
         imageCapture
@@ -54,7 +80,33 @@ window.onload = () => {
             readBarCode(image);
           })
           .catch((error) => console.error("takePhoto() error:", error));
-      })
-      .catch((error) => console.error("getUserMedia() error:", error));
-  });
+      });
+    });
+}
+
+function captureImage() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((mediaStream) => {
+      console.log(mediaStream);
+      const imageCapture = gotMedia(mediaStream);
+      const image = document.getElementById("imageEl");
+
+      imageCapture
+        .takePhoto()
+        .then((blob) => {
+          image.src = URL.createObjectURL(blob);
+          image.onload = () => {
+            URL.revokeObjectURL(this.src);
+          };
+
+          readBarCode(image);
+        })
+        .catch((error) => console.error("takePhoto() error:", error));
+    })
+    .catch((error) => console.error("getUserMedia() error:", error));
+}
+
+window.onload = () => {
+  captureVideo();
 };
