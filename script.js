44b4263b281bac9e1fa38399ab0f36a9a0e200ba
Gabriel Silva <gabriel.nascimento@koin.com.br>
2022-02-02 01:29:28 -0300

    take photo and read barcode

--- script.js
+++ script.js
@@ -1,28 +1,64 @@
 window.onload = () => {
-  if (!("BarcodeDetector" in window)) {
-    console.log("Barcode Detector is not supported by this browser.");
-  } else {
-    console.log("Barcode Detector supported!");
-
-    // create new detector
-    var barcodeDetector = new BarcodeDetector({
-      formats: ["code_39", "codabar", "ean_13"],
-    });
-
-    barcodeDetector
-      .detect(document.getElementById("imageEl"))
-      .then((barcodes) => {
-        console.log("barcodes foundeds");
-        console.log(barcodes);
-
-        barcodes.forEach((barcode) => {
-          console.log(barcode.rawData);
-
-          document.getElementById("barcode-data").innerHTML = barcode.rawData;
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  function gotMedia(mediaStream) {
+    const mediaStreamTrack = mediaStream.getVideoTracks()[0];
+    const imageCapture = new ImageCapture(mediaStreamTrack);
+    return imageCapture;
   }
+
+  function readBarCode(dataUrl) {
+    if (!("BarcodeDetector" in window)) {
+      console.log("Barcode Detector is not supported by this browser.");
+    } else {
+      console.log("Barcode Detector supported!");
+
+      // create new detector
+      var barcodeDetector = new BarcodeDetector({
+        formats: ["code_39", "codabar", "ean_13"],
+      });
+
+      barcodeDetector
+        .detect(dataUrl)
+        .then((barcodes) => {
+          console.log("barcodes foundeds");
+          console.log(barcodes);
+
+          barcodes.forEach((barcode) => {
+            console.log(barcode.rawData);
+
+            document.getElementById("barcode-data").innerHTML = barcode.rawData;
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+
+  document.getElementById("capture").addEventListener("click", () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((mediaStream) => {
+        const imageCapture = gotMedia(mediaStream);
+        const image = document.getElementById("imageEl");
+
+        imageCapture
+          .takePhoto()
+          .then((blob) => {
+            const fr = new FileReader();
+            fr.readAsDataURL(blob);
+
+            fr.onloadend = () => {
+              readBarCode(fr.result);
+            };
+
+            const src = URL.createObjectURL(blob);
+            image.src = src;
+            image.onload = () => {
+              URL.revokeObjectURL(this.src);
+            };
+          })
+          .catch((error) => console.error("takePhoto() error:", error));
+      })
+      .catch((error) => console.error("getUserMedia() error:", error));
+  });
 };
